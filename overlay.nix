f6699a1c2d96d96e1bdc6273b62ee5e53f02070e
madosuki <yurikasamaaikatsu@gmail.com>
2026-02-19 15:12:42 +0900

    add edcb

--- overlay.nix
+++ overlay.nix
@@ -2,4 +2,5 @@ self: super: {
   jdim = super.callPackage ./packages/jdim/default.nix {};
   libaribb25-tsukumijima = super.callPackage ./packages/libaribb25-tsukumijima/default.nix {};
   recisdb = super.callPackage ./packages/recisdb/default.nix {};
+  edcb = super.callPackage ./packages/edcb/default.nix {};
 }
---
+++ packages/edcb/default.nix
@@ -0,0 +1,101 @@
+{ pkgs, stdenv, fetchFromGitHub
+, bitrateIni ? null
+, bonCtrlIni ? null
+, contentTypeText ? null
+, epgTimerSrvIni ? null }:
+
+stdenv.mkDerivation rec {
+  pname = "edcb";
+  version = "251101";
+  buildInputs = [
+    pkgs.openssl
+    pkgs.zlib
+    pkgs.lua52Packages.lua
+    pkgs.lua52Packages.lua-zlib
+    stdenv.cc.cc.lib
+  ];
+  nativeBuildInputs = [
+    pkgs.gcc
+    pkgs.pkg-config
+    pkgs.curl
+  ];
+  src = fetchFromGitHub {
+    owner = "xtne6f";
+    repo = "EDCB";
+    rev = "d21fa8a8fa36c8366e366fcd200e01232a9a8d60";
+    sha256 = "sha256-VfhXRinvlN/n54NsqsNn/rhlohxY91zoV92nMi82AvU=";
+  };
+  # sourceRoot = "${src.name}/Document/Unix";
+
+  postPatch = ''
+    # replace /var/local/edcb to $out/etc/edcb; edcb is hard-coding use /var/local/edcb.
+    find . -type f \( -name "*.cpp" -o -name "*.h" \) -exec sed -i 's|/var/local/edcb|/etc/edcb|g' {} +
+  '';
+
+  preBuild = ''
+    export NIX_LDFLAGS="$NIX_LDFLAGS -L${pkgs.lua5_2}/lib -llua"
+    
+    # replace -llua5.2 in Makefile.
+    find . -name Makefile -exec sed -i 's/-llua5.2/-llua/g' {} +
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin
+    mkdir -p $out/lib
+    cd ../../
+
+    # cp ./EpgDataCap3/EpgDataCap3/EpgDataCap3.so $out/lib/
+    cp ./EpgDataCap3/EpgDataCap3/EpgDataCap3_Unicode.so $out/lib/libEpgDataCap3.so
+    cp ./RecName_Macro/RecName_Macro/RecName_Macro.so $out/lib/
+    cp ./SendTSTCP/SendTSTCP/SendTSTCP.so $out/lib/
+    cp ./Write_Default/Write_Default/Write_Default.so $out/lib/
+
+    cp ./EpgDataCap_Bon/EpgDataCap_Bon/EpgDataCap_Bon $out/bin/
+    cp ./EpgTimerSrv/EpgTimerSrv/EpgTimerSrv $out/bin/
+
+    # install setting files
+    mkdir -p $out/etc/edcb
+
+    ${if bitrateIni != null
+      then "cp ${bitrateIni} $out/etc/edcb/Bitrate.ini"
+      else "iconv -f CP932 -t UTF-8 ./ini/Bitrate.ini | tr -d '\r' > $out/etc/edcb/Bitrate.ini"
+     }
+    ${if bonCtrlIni != null
+      then "cp ${bonCtrlIni} $out/etc/edcb/BonCtrl.ini"
+      else "iconv -f CP932 -t UTF-8 ./ini/BonCtrl.ini | tr -d '\r' | sed 's/\.dll$$/.so/' > $out/etc/edcb/BonCtrl.ini"
+     }
+
+    ${if contentTypeText != null
+      then "cp ${contentTypeText} $out/etc/edcb/ContentTypeText.txt"
+      else "tr -d '\r' < ./ini/ContentTypeText.txt > $out/etc/edcb/ContentTypeText.txt"
+     }
+
+
+    mkdir -p $out/etc/edcb/HttpPublic
+    cp -Rn ./ini/HttpPublic/index.html ./ini/HttpPublic/favicon.ico ./ini/HttpPublic/legacy $out/etc/edcb/HttpPublic/
+    ${if epgTimerSrvIni != null
+      then "cp ${epgTimerSrvIni} $out/etc/edcb/HttpPublic/"
+      else "echo '[SET]' > $out/etc/edcb/EpgTimerSrv.ini; \
+      	    echo 'EnableHttpSrv=2' > $out/etc/edcb/EpgTimerSrv.ini; \
+	          echo 'HttpAccessControlList=+127.0.0.1,+::1,+::ffff:127.0.0.1' >> $out/etc/edcb/EpgTimerSrv.ini"
+     }
+
+    runHook postInstall
+  '';
+
+  buildPhase = ''
+    runHook preBuild
+    cd Document/Unix
+    make NOPCH=1 -j $NIX_BUILD_CORES
+    runHook postBuild
+  '';
+
+  meta = with pkgs.lib; {
+    description = "EDCB (EpgDataCap_Bon) forked by xtne6f";
+    homepage = "https://github.com/xtne6f/EDCB";
+    # license = licenses.mit; # 実際のライセンスを確認してください
+    platforms = platforms.linux;
+  };
+}
