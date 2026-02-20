00fb0800479186e4c02b202eb60df0c9bc0327fc
madosuki <yurikasamaaikatsu@gmail.com>
2026-02-20 16:18:40 +0900

    add packages/mirakc

--- overlay.nix
+++ overlay.nix
@@ -3,4 +3,5 @@ self: super: {
   libaribb25-tsukumijima = super.callPackage ./packages/libaribb25-tsukumijima/default.nix {};
   recisdb = super.callPackage ./packages/recisdb/default.nix {};
   edcb = super.callPackage ./packages/edcb/default.nix {};
+  mirakc = super.callPackage ./packages/mirakc/default.nix {};
 }
---
+++ packages/mirakc/default.nix
@@ -0,0 +1,51 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.rustPlatform.buildRustPackage rec {
+  pname = "mirakc";
+  version = "3.4.56";
+
+  swaggerUiZip = pkgs.fetchurl {
+    url = " https://github.com/swagger-api/swagger-ui/archive/refs/tags/v5.17.14.zip";
+    hash = "sha256-SBJE0IEgl7Efuu73n3HZQrFxYX+cn5UU5jrL4T5xzNw=";
+  };
+
+  src = pkgs.fetchFromGitHub {
+    owner = "mirakc";
+    repo = "mirakc";
+    rev = "3b64a2ef73736ed291c406127f4e0d0bcf3c87a1"; 
+    fetchSubmodules = true;
+    sha256 = "sha256-JpGEm0Hsy3FRapa/rcDVIzfaEBmBNyRaBt0Bmt6V9IU=";
+  };
+
+  cargoLock = {
+    lockFile = "${src}/Cargo.lock";
+  };
+
+  nativeBuildInputs = with pkgs; [
+    curl
+    rustPlatform.bindgenHook
+  ];
+
+  buildInputs = with pkgs; [
+  ];
+
+  preBuild = ''
+  cp ${swaggerUiZip} ./swagger-ui.zip
+  chmod 755 ./swagger-ui.zip
+  export SWAGGER_UI_DOWNLOAD_URL="file://$(pwd)/swagger-ui.zip"
+  '';
+
+  checkFlags = [
+    "--skip=config::tests::test_load"
+  ];
+
+  meta = with pkgs.lib; {
+    description = "mirakc";
+    homepage = "https://github.com/mirakc/mirakc";
+    license = with licenses; [
+      asl20
+      mit
+    ];
+    platforms = platforms.linux;
+  };
+}
